3bdc8f9c0f2378be099ed811a556867bbbe1aba5
TheFakeBox <roryscomp5@gmail.com>
2024-05-04 16:16:36 -0400

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -39,7 +39,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Hi! I'm TheRealBox. I play video games and enjoy playing tennis, and am an experienced Discord app developer!`)
+            .setDescription(`__**About me**__:\n\n ▶️ Hi! I'm TheRealBox. I play video games and enjoy playing tennis, and am an experienced Discord app developer! I made this bot with some code from a YouTuber, so I linked his social with the /help command!`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
